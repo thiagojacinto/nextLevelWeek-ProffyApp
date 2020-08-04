9af404f6ee9749a2ba2fff5887a04a33efe8e979
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-08-04 00:21:31 -0300

    [page] select instructor

--- web/src/pages/SelectInstructor/index.tsx
+++ web/src/pages/SelectInstructor/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+// import { Link } from "react-router-dom";
+import Header from "../../components/Header";
+
+export const InstructorSelectionPage = styled.div`
+  min-width: 100vw;
+  height: 100vh;
+`;
 
 export const SelectInstructor: React.FC = () => {
-  return <h1>Select Instructor</h1>;
+  return (
+    <InstructorSelectionPage className="container">
+      <Header>
+        <strong>These are the available instructors.</strong>
+      </Header>
+    </InstructorSelectionPage>
+  );
 };
 
 export default SelectInstructor;
