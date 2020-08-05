40fb133905918bbfba8a71d3cc1263938efddae1
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-08-05 14:46:45 -0300

    [routes] server routes integration

---
+++ server/src/Routes/index.ts
@@ -0,0 +1,31 @@
+import { ServerRoute } from "@hapi/hapi";
+import { RegisterInstructor } from "./RegisterInstructor";
+
+export const errorMessage = "An error has ocurried. Please try again later.";
+
+export const Routes: ServerRoute[] = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      request.log("error", errorMessage);
+      return h.response("Hello World!");
+    },
+  },
+
+  {
+    method: "POST",
+    path: "/courses",
+    handler: RegisterInstructor,
+  },
+
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: function (request, h) {
+      return "404 Error! Page Not Found!";
+    },
+  },
+];
+
+export default Routes;
