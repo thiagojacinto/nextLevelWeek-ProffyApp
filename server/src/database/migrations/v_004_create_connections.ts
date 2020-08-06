5c149ef9e8b1e11c3a8d2077a811b0fae993c902
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-08-06 00:20:05 -0300

    [fix] sqlite3 auto timestamp creation

--- server/src/database/migrations/v_004_create_connections.ts
+++ server/src/database/migrations/v_004_create_connections.ts
@@ -3,7 +3,10 @@ import Knex from "knex";
 export async function up(knex: Knex) {
   return knex.schema.createTable("connections", (table) => {
     table.increments("id").primary().unique();
-    table.timestamp("created_at").defaultTo("now()").notNullable();
+    table
+      .timestamp("created_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP"))
+      .notNullable();
     table
       .integer("instructor_id")
       .notNullable()
