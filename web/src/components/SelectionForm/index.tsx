dc2cb92c1d484f342daf3de0fe296e7541801825
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-08-07 00:28:36 -0300

    [refactor] exporting input to a new component

--- web/src/components/SelectionForm/index.tsx
+++ web/src/components/SelectionForm/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Input from "../Input";
 
 export const Form = styled.form`
   fieldset {
@@ -10,38 +11,6 @@ export const Form = styled.form`
 
   label {
     color: var(--color-text-in-primary);
-  }
-
-  .input-block {
-    position: relative;
-  }
-
-  .input-block label {
-    font-size: 1.6rem;
-  }
-
-  .input-block input {
-    width: 100%;
-    height: 5.6rem;
-    margin-top: 0.8rem;
-    border-radius: 0.8rem;
-    background-color: var(--color-input-background);
-    border: 1px solid var(--color-line-in-white);
-    outline: 0;
-    padding: 0 1.6rem;
-    font: 1.6rem "Noto Sans JP";
-    box-shadow: 0px 2px 3px var(--color-primary-darker);
-  }
-
-  .input-block:focus-within::after {
-    width: calc(100% - 3.2rem);
-    height: 2px;
-    content: "";
-    background-color: var(--color-primary-light);
-    position: absolute;
-    left: 1.6rem;
-    right: 1.6rem;
-    bottom: 0;
   }
 
   @media (min-width: 700px) {
@@ -58,20 +27,9 @@ type uniqueId = { id: string };
 export const SelectionForm: React.FC<uniqueId> = ({ id }: uniqueId) => {
   return (
     <Form id={id}>
-      <fieldset className="input-block">
-        <label htmlFor="subject">Subject</label>
-        <input type="text" name="subject" id="subject" />
-      </fieldset>
-
-      <fieldset className="input-block">
-        <label htmlFor="week_day">Weekday</label>
-        <input type="text" name="week_day" id="week_day" />
-      </fieldset>
-
-      <fieldset className="input-block">
-        <label htmlFor="time">Time</label>
-        <input type="text" name="time" id="time" />
-      </fieldset>
+      <Input name="subject" label="Subject" />
+      <Input name="week_day" label="Week day" />
+      <Input type="time" name="time" label="Time" />
     </Form>
   );
 };
